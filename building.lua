6ab6ad6372ea38a0c370bda9c07bfc99f528c014
bjorn <tie.372@gmail.com>
2015-08-03 19:40:43 -0700

    Wood louder;

--- building.lua
+++ building.lua
@@ -101,6 +101,6 @@ function Building:destroy()
   self.body:setAngularVelocity(-20 + love.math.random() * 40)
   ctx.hud:addScore(50, 'building')
   ctx.sound:play('wood', function(sound)
-    sound:setVolume(.5)
+    sound:setVolume(1)
   end)
 end
